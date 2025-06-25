d01e8fdd298e911ba940749ccc654400b70a0244
aiueoxyz <k023g0028@g.neec.ac.jp>
2025-06-25 12:40:21 +0900

    08

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,16 +1,17 @@
-#include "IndexBuffer.h"
 #include "KamataEngine.h"
-#include "PipelineState.h"
-#include "RootSignature.h"
-#include "Shader.h"
-#include "VertexBuffer.h"
 #include <Windows.h>
 #include <cassert>
+#include "Shader.h"
+#include "RootSignature.h"
+#include "PipelineState.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
 
 using namespace KamataEngine;
 
 // 関数プロトタイプ宣言
 void SetupPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader& vs, Shader& ps);
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -29,23 +30,27 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// DirectXCommonクラスが管理している、コマンドリストの取得
 	ID3D12GraphicsCommandList* commandList = dxCommon->GetCommandList();
 
+
 	// リソースの確保含め、頂点情報を柔軟に対応できるように VertexData構造体を新たに作成する
 	// Vertex4 → VertexData に変更して利用する
 	struct VertexData {
 		Vector4 position;
+		Vector2 texcoord;
 	};
 
 	// 頂点データの準備
 	VertexData vertices[] = {
-	    {-1.0f, 1.0f,  0.0f, 1.0f}, // 左上
-	    {1.0f,  1.0f,  0.0f, 1.0f}, // 右上
-	    {-1.0f, -1.0f, 0.0f, 1.0f}, // 左下
-	    {1.0f,  -1.0f, 0.0f, 1.0f}, // 右下
+	    {{-1.0f, 1.0f, 0.0f, 1.0f}, {0.5f, 0.5f}} , // 左上
+	    {{1.0f, 1.0f, 0.0f, 1.0f}, {0.5f, 0.5f}}, // 右上
+	    {{-1.0f, -1.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},  // 左下
+	    {{1.0f, -1.0f, 0.0f, 1.0f},  {1.0f, 1.0f}},  // 右下
 	};
+
 
 	// RootSignature作成 ------------------------------------
 	RootSignature rs;
 	rs.Create();
+
 
 	// 頂点シェーダの読み込みとコンパイル
 	Shader vs;
@@ -57,9 +62,11 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	ps.LoadDxc(L"Resources/shaders/TestPS.hlsl", L"ps_6_0");
 	assert(ps.GetDxcBlob() != nullptr);
 
+
 	// PipelineState作成 ----------------------------------------------------------------------------------------------------
 	PipelineState pipelineState;
 	SetupPipelineState(pipelineState, rs, vs, ps);
+
 
 	// VertexBuffer(VertexResource, VertexResourceView)の生成
 	VertexBuffer vb;
@@ -73,9 +80,15 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		pGpuVertices[i] = vertices[i];
 	}
 
+
 	// 頂点インデックスデータの準備
 	uint16_t indices[] = {
-	    0, 1, 2, 1, 3, 2,
+	    0,
+	    1,
+	    2,
+		1,
+		3,
+		2,
 	};
 
 	// IndexBuffer(IndexResource, IndexResourceView)の生成
@@ -90,6 +103,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		pGpuIndices[i] = indices[i];
 	}
 
+
 	// メインループ
 	while (true) {
 		// エンジンの更新
@@ -101,10 +115,10 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		dxCommon->PreDraw();
 
 		// コマンドを積む
-		commandList->SetGraphicsRootSignature(rs.Get());     // RootSignatureの設定
-		commandList->SetPipelineState(pipelineState.Get());  // PSOの設定する
-		commandList->IASetVertexBuffers(0, 1, vb.GetView()); // VBVの設定する
-		commandList->IASetIndexBuffer(ib.GetView());         // IBVの設定する
+		commandList->SetGraphicsRootSignature(rs.Get());         // RootSignatureの設定
+		commandList->SetPipelineState(pipelineState.Get());      // PSOの設定する
+		commandList->IASetVertexBuffers(0, 1, vb.GetView());     // VBVの設定する
+		commandList->IASetIndexBuffer(ib.GetView());             // IBVの設定する
 		// トポロジの設定
 		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
@@ -123,11 +137,16 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 void SetupPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader& vs, Shader& ps) {
 	// InputLayout
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[2] = {};
 	inputElementDescs[0].SemanticName = "POSITION";
 	inputElementDescs[0].SemanticIndex = 0;
 	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
 	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+
+	inputElementDescs[1].SemanticName = "TEXCOORD";
+	inputElementDescs[1].SemanticIndex = 0;
+	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
 
 	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
 	inputLayoutDesc.pInputElementDescs = inputElementDescs;
@@ -139,11 +158,11 @@ void SetupPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader&
 	blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
 
 	// RasterizerState ---------------------------------------
-	D3D12_RASTERIZER_DESC rasterizerDesc{};
+	 D3D12_RASTERIZER_DESC rasterizerDesc{};
 	// 裏面(反時計回り)をカリングする
-	rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
+	 rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
 	// 塗りつぶしモードをソリッドにする(ワイヤーフレームなら D3D12_FILL_MODE_WIREFRAME)
-	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
+	 rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
 
 	// PSO(PipelineStateObject)の生成 ---------------------------
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc{};
